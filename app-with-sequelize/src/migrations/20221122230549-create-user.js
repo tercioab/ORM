7bde59dc8498f3d32ea3a34a72414c42824fa86d
walthercio Almeida <suaagenciainicial@gmail.com>
2022-11-22 20:31:13 -0300

    feat: add fiels on createTable of users on migrations

--- app-with-sequelize/src/migrations/20221122230549-create-user.js
+++ app-with-sequelize/src/migrations/20221122230549-create-user.js
@@ -1,14 +1,33 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+     await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+ 
   },
 
   async down (queryInterface, Sequelize) {
