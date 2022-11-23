796d5401568d313956cda5e38b47df6d264bc51a
walthercio Almeida <suaagenciainicial@gmail.com>
2022-11-22 23:20:51 -0300

    feat: controller to find user by id and email

--- app-with-sequelize/src/controllers/user.controller.js
+++ app-with-sequelize/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const UserService = require('../services/user.service');
 
-const ERROR_MESSAGE = 'Algo deu errado'
+const ERROR_MESSAGE = 'Algo deu errado';
+const ERROR_FIND_USER = 'Usuario não encontrado';
 
 const getAll = async (_req, res) => {
     try {
@@ -17,7 +18,7 @@ const getById = async (req, res) => {
     try {
         const {id} = req.params
         const userId = await UserService.getById(id)
-        if (!userId) return res.status(404).json({ message: 'Usuario não encontrado' });
+        if (!userId) return res.status(404).json({ message: ERROR_FIND_USER });
         return res.status(201).json(userId)
 
     } catch (e) {
@@ -26,7 +27,21 @@ const getById = async (req, res) => {
     }
 }
 
+const getByIdAndEmail = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { email } = req.query;
+        const user = await UserService.getByIdAndEmail(id, email);
+        if (!user) return res.status(404).json({ message: ERROR_FIND_USER });
+        return res.status(201).json(user);
+    } catch (e) {
+        console.log(e.message); 
+        res.status(500).json({message: ERROR_MESSAGE})
+    }
+}
+
 module.exports = {
     getAll,
     getById,
+    getByIdAndEmail,
 }
