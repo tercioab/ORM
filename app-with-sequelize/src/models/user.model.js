fd85a10e541675a0a129638af31bc8c545b48c02
walthercio Almeida <suaagenciainicial@gmail.com>
2022-11-22 22:20:51 -0300

    withdraw comments

--- app-with-sequelize/src/models/user.model.js
+++ app-with-sequelize/src/models/user.model.js
@@ -1,4 +1,4 @@
-const UserModel = (sequelize, DataTypes) => {
+const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
@@ -8,4 +8,4 @@ const UserModel = (sequelize, DataTypes) => {
   return User;
 };
 
-module.exports = UserModel;
+module.exports = User;
